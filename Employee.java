ecb14e3cfa265e59d925332ab9bae76d6135d316
polivetrova <itspollyv@gmail.com>
2020-12-08 23:57:55 +0300

    удалены ненужные строки

--- Employee.java
+++ Employee.java
@@ -19,17 +19,15 @@ public class Employee {
 
     }
 
-    protected void printEmployee(/*String fullName,String occupation, String email, String phoneNum, int salary, int age*/){
+    protected void printEmployee(){
         System.out.printf("ФИО сотрудника: %S.%nДолжность: %s. %nEmail: %s%nНомер телефона: %s%nОклад: %d руб.%n",fullName, occupation, email, phoneNum, salary);
         if(age % 10 == 1) {
             System.out.printf("Возраст: %d год.%n", age);
-            System.out.println();
         } else if (age % 10 >= 2 && age % 10 <= 4){
             System.out.printf("Возраст: %d года.%n", age);
-            System.out.println();
         } else {
             System.out.printf("Возраст: %d лет.%n", age);
-            System.out.println();
         }
+        System.out.println();
     }
 }
